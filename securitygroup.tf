34b507a391bab0dc85e871f7f9a2584824450d97
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-30 14:20:13 +0100

    Create security group resource

---
+++ securitygroup.tf
@@ -0,0 +1,36 @@
+resource "aws_security_group" "allow_elk" {
+  name        = "allow_elk"
+  description = "Elasticsearch traffic"
+
+  # elasticsearch port
+  ingress {
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # logstash port
+  ingress {
+    from_port   = 5043
+    to_port     = 5044
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # kibana ports
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
