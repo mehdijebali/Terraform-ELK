04a06bfef02e4da03ac145a6f205bcd6fb790d98
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-12 16:43:27 +0100

    Set ELK installation in linux2

--- installELK.sh
+++ installELK.sh
@@ -4,45 +4,37 @@ sudo yum update
 sudo yum install java-1.8.0 -y
 sudo java -version
 
+# import ELK repository
+sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+
 # install elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-x86_64.rpm
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.15.2-x86_64.rpm.sha512
-
-sha512sum -c elasticsearch-7.15.2-x86_64.rpm.sha512
-
-sudo rpm --install elasticsearch-7.15.2-x86_64.rpm
-sleep 10
+cat <<EOF > /etc/yum.repos.d/elasticsearch.repo
+[elasticsearch-8.x]
+name=Elasticsearch repository for 8.x packages
+baseurl=https://artifacts.elastic.co/packages/8.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+sudo yum -y install elasticsearch
 sudo mv /tmp/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
+sleep 10
 
 # install logstash
-sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
-cat << EOF > /etc/yum.repos.d/logstash.repo
-    [logstash-8.x]
-    name=Elastic repository for 8.x packages
-    baseurl=https://artifacts.elastic.co/packages/8.x/yum
-    gpgcheck=1
-    gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
-    enabled=1
-    autorefresh=1
-    type=rpm-md
-EOF
-sudo yum install logstash
+sudo yum -y install logstash
+sudo mv /tmp/apache-01.conf /etc/logstash/conf.d/apache-01.conf
+sudo systemctl enable logstash
 sleep 10
 
 # install kibana
-sudo apt-get install kibana
-sleep 10
-
+sudo yum -y install kibana
 sudo mv /tmp/kibana.yml /etc/kibana/kibana.yml
 sudo systemctl enable kibana
+sleep 10
 
 # install filebeats
-sudo apt-get install metricbeat
-sleep 10
-sudo systemctl enable metricbeat
-
-# Start LogStash
-sudo mv /tmp/apache-01.conf /etc/logstash/conf.d/apache-01.conf
-sleep 10
-sudo systemctl enable logstash
+sudo yum -y install filebeat
+sudo filebeat modules enable system
