02bfc45a5b94c8b2af2baf81d1dae64e671b411e
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-12 15:33:15 +0100

    Configure logstach installation

--- installELK.sh
+++ installELK.sh
@@ -16,7 +16,18 @@ sudo mv /tmp/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
 
 # install logstash
-sudo apt-get install logstash
+sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cat << EOF > /etc/yum.repos.d/logstash.repo
+    [logstash-8.x]
+    name=Elastic repository for 8.x packages
+    baseurl=https://artifacts.elastic.co/packages/8.x/yum
+    gpgcheck=1
+    gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+    enabled=1
+    autorefresh=1
+    type=rpm-md
+EOF
+sudo yum install logstash
 sleep 10
 
 # install kibana
