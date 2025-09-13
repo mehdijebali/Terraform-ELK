c2eb5fd3996157d0b4b8e704148052dd4c74fd14
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-09-13 20:50:01 +0200

    📝 Feat/Format Terraform file

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,65 +1,65 @@
 variable "AWS_REGION" {
-  type = string
+  type        = string
   description = "The region where resources are deployed"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 variable "PATH_TO_PUBLIC_KEY" {
-  type = string
+  type        = string
   description = "Path to SSH public key"
-  default = "levelup_key.pub"
+  default     = "levelup_key.pub"
 }
 
 variable "PATH_TO_PRIVATE_KEY" {
-  type = string
+  type        = string
   description = "Path to SSH private key"
-  default = "levelup_key"
+  default     = "levelup_key"
 }
 
 variable "INSTANCE_USERNAME" {
-  type = string
+  type        = string
   description = "AWS instance username used for SSH connection"
-  default = "ubuntu"
+  default     = "ubuntu"
 }
 
 variable "INSTANCE_TYPE" {
-  type = string
+  type        = string
   description = "AWS Instance type"
-  default = "m4.large"
+  default     = "m4.large"
 }
 
 variable "AVAILABILITY_ZONE" {
-  type = string
+  type        = string
   description = "AWS AZ where ELK instance is deployed"
-  default = "us-east-1a"
+  default     = "us-east-1a"
 }
 
 variable "SG_NAMES" {
-  type    = list(any)
+  type        = list(any)
   description = "Security Group names of ELK instance"
-  default = ["allow-levelup-ssh", "allow_elk"]
+  default     = ["allow-levelup-ssh", "allow_elk"]
 }
 
 variable "SG_DESCRIPTIONS" {
-  type    = list(any)
+  type        = list(any)
   description = "SG descriptions"
-  default = ["security group that allows ssh connection", "Elasticsearch traffic"]
+  default     = ["security group that allows ssh connection", "Elasticsearch traffic"]
 }
 
 variable "INSTANCE_NAME" {
-  type = string
+  type        = string
   description = "The name of EC2 instance"
-  default = "ELK Server"
+  default     = "ELK Server"
 }
 
 variable "KEY_NAME" {
-  type = string
+  type        = string
   description = "SSH key name"
-  default = "tf-ssh-key"
+  default     = "tf-ssh-key"
 }
 
 variable "LD_NAME" {
-  type = string
+  type        = string
   description = "Linux distribution"
-  default = "centos"
+  default     = "centos"
 }
