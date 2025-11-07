05579f853d2905d73a696d517f6b8b6fe5c1b1d5
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2025-11-07 09:57:36 +0100

    🔥 Feat/Remove local modules

--- terraform/modules/instance/3-variables.tf
+++
@@ -1,8 +0,0 @@
-variable "SG_VPC_ID" {}
-variable "SG_NAME" {}
-variable "SG_DESCRIPTION" {}
-variable "AMI_ID" {}
-variable "INSTANCE_TYPE" {}
-variable "INSTANCE_NAME" {}
-variable "AVAILABILITY_ZONE" {}
-variable "VPC_SECURITY_GROUP_IDS" {}
